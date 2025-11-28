d62c0e1fd3329a21c64d9e70b1660e480454994c
Vinícius Mergulhão <113155890+Vinimtt@users.noreply.github.com>
2025-11-28 17:56:39 +0000

    add: paginas de tabs

--- portifolio-mobile/app/_layout.tsx
+++ portifolio-mobile/app/_layout.tsx
@@ -1,24 +1,62 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
+export default function Layout() {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Tabs screenOptions={{ tabBarActiveTintColor: '#007AFF' }}>
+      
+      <Tabs.Screen 
+        name="sobre" 
+        options={{ 
+          title: 'Sobre',
+          tabBarIcon: ({ color }) => <Ionicons name="information-circle" size={24} color={color} />
+        }} 
+      />
+      <Tabs.Screen 
+        name="academico" 
+        options={{ 
+          title: 'Acadêmico',
+          tabBarIcon: ({ color }) => <Ionicons name="school" size={24} color={color} />
+        }} 
+      />
+
+      <Tabs.Screen 
+        name="index" 
+        options={{ 
+          title: 'home',
+          headerShown: false,
+          tabBarIcon: ({ color }) => <Ionicons name="home" size={24} color={color} />
+        }} 
+      />
+
+      <Tabs.Screen 
+        name="profissional" 
+        options={{ 
+          title: 'Profissional',
+          tabBarIcon: ({ color }) => <Ionicons name="briefcase" size={24} color={color} />
+        }} 
+      />
+      <Tabs.Screen 
+        name="projetos" 
+        options={{ 
+          title: 'Projetos',
+          tabBarIcon: ({ color }) => <Ionicons name="code-slash" size={24} color={color} />
+        }} 
+      />
+      <Tabs.Screen 
+        name="jogo" 
+        options={{ 
+          title: 'Jogo',
+          headerShown: false,
+          tabBarIcon: ({ color }) => <Ionicons name="game-controller" size={24} color={color} />
+        }} 
+      />
+
+      <Tabs.Screen name="explore" options={{ href: null }} />
+      <Tabs.Screen name="(tabs)" options={{ href: null }} />
+      <Tabs.Screen name="modal" options={{ href: null }} />
+      <Tabs.Screen name="+not-found" options={{ href: null }} />
+      
+    </Tabs>
   );
 }
